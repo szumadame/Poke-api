92bda2fce457fa61818fe73f0d2f2226d2b15659
szumadame <szumada.a@gmail.com>
2022-10-25 18:31:20 +0200

    first commit

---
+++ src/main/java/com/example/pokemons/PokemonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonsApplication.class, args);
+	}
+
+}
